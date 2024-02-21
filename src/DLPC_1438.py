b3589439f420487368d70333505f064384a5dfed
Nemo Andrea <nemoandrea@gmail.com>
2024-02-22 00:03:07 +0100

    Add intial external print mode code
    Adds basic configuration options for EXTERNAL PRINT mode, as extracted from the I2C signals from the elegoo board.

--- src/DLPC_1438.py
+++ src/DLPC_1438.py
@@ -8,6 +8,7 @@ import enum
 PROJ_ON = 5
 SYS_RDY = 6
 HOST_IRQ = 19
+CSZ = 8
 
 #  DLPC1438 on Anyubic board address
 DLPC1438_addr = 0x1B
@@ -17,6 +18,7 @@ class Mode(enum.IntEnum):
     EXTERNALPRINT = 0x06,
     TESTPATTERN = 0x01
 
+## TODO: consider making this object oriented with DLCP1438 class
 
 def intialise_DLPC1438(i2c_bus):
     """
@@ -36,10 +38,11 @@ def intialise_DLPC1438(i2c_bus):
     # [1] host_IRQ is low, and the DLPC1438 has not started yet
     # [2] host IRQ is low, but the DLCP1438 is already active and running
 
-    if i2c_bus.read_byte(DLPC1438_addr) != 0:  # check if i2c is already active. Then we are in [2]
+    try:  # we try to communicate over I2C. This will trow I/O error if DLPC1438 is not active yet
+        i2c_bus.read_byte(DLPC1438_addr)  # check if i2c is already active. Then we are in [2]
         warnings.warn("DLPC1438 is already powered on when script initialised. No startup action needed.")
         return
-    else:  # DCLP1438 seems to still be turned off (case [1]). Let's start it
+    except IOError:  # DCLP1438 seems to still be turned off (case [1]). Let's start it
         print("Setting PROJ_ON high to start DLPC1438 startup sequence... ")
 
         # send the PROJ_ON signal and wait for the HOST_IRQ signal to go high, signalling the DLPC1438
@@ -47,7 +50,7 @@ def intialise_DLPC1438(i2c_bus):
         GPIO.output(PROJ_ON, GPIO.HIGH)
         ref_time = time.time()
         while not GPIO.input(HOST_IRQ):
-            time.sleep(0.1)
+            time.sleep(0.01)
 
         print(f"DLPC1438 is signalling it is ready for use, {time.time()-ref_time} seconds after PROJ_ON went high.")
 
@@ -62,16 +65,70 @@ def intialise_DLPC1438(i2c_bus):
 
 
 def switch_mode(new_mode, i2c_bus):
+    """
+    Switch between DLPC1438 operational modes.
+
+    Note that the input of the function must be an Enum of the Mode class.
+    """
     # check that the new mode is actually a valid enum entry 
     if isinstance(new_mode, Mode):  
         print(f"> Switching DLPC1438 mode to {new_mode.name}\t (currently in {i2c_bus.read_i2c_block_data(DLPC1438_addr,0x06,1)[0]})")
 
         i2c_bus.write_i2c_block_data(DLPC1438_addr, 0x05, [new_mode])  # send the new mode setting
 
-        time.sleep(0.3)  # need some time to switch between modes
+        time.sleep(0.4)  # need some time to switch between modes
 
         # check that the mode switch was successful
         assert i2c_bus.read_i2c_block_data(DLPC1438_addr,0x06,1)[0] == new_mode.value, f"Was unable to switch the DLPC 1438 to MODE:{new_mode}"
 
     else:
         raise Exception("Invalid DLPC1438 mode provided. Use the Enum 'Mode', rather than the hex value.")
+    
+
+def configure_external_print(LED_PWM, i2c_bus):
+    """
+    Apply various settings applicable to the EXTERNAL_PRINT mode of the DLPC1438.
+
+    This function will configure multiple registers. Note that switching to STANDBY mode will
+    likely reset some of these values, so it is suggested to call this just before switching
+    to EXTERNAL_PRINT mode.
+    """
+    print("\nConfiguring the DLPC1438 for external print mode...")
+
+    # Write LED PWM current (0x54)
+    # Set PWM value for LED 3/1
+    i2c_bus.write_i2c_block_data(DLPC1438_addr, 0x54, [0xBE, 0x01, 0x00, 0x00, 0x64, 0x00])  # TODO: figure out if we are dealing with LED1 or LED3, set other to 0
+
+    # Write FPGA control (0xCA)
+    # no CRC error injection, no CRC error calc, no FGPA reset, no FPGA reset unlock
+    i2c_bus.write_i2c_block_data(DLPC1438_addr, 0xCA, [0b00000000])  
+    
+    # write external print confugration (0xA8)
+    # we choose a linear transfer function (byte 1) and select led 1 (byte 2)
+    i2c_bus.write_i2c_block_data(DLPC1438_addr, 0xA8, [0x00, 0b00000001])  
+
+    # Write parallel video (0xC3)
+    # Enable the FPGA parallel video interface.
+    i2c_bus.write_i2c_block_data(DLPC1438_addr, 0xC3, [0x01])  
+
+    # Write actuator orientation (0xC8)  # TODO: figure out if this one is really needed
+    # see DLPC1438 programming guide for interpetation of these bytes. For now just using values of Elegoo board.
+    i2c_bus.write_i2c_block_data(DLPC1438_addr, 0xC8, [0x03, 0x16, 0x11, 0x06, 0x01])  
+
+
+def  await_SYS_RDY():
+    while not GPIO.input(SYS_RDY):
+        time.sleep(0.3)
+        print("waiting for SYS_RDY")
+
+
+def expose_pattern(exposure_time,i2c_bus):
+    assert GPIO.input(SYS_RDY), "SYS_RDY signal is not high yet, cannot expose frames yet."
+
+    # Write External Print Control (C1)
+    # start exposure of current buffer for infinite duration with 5 dark frames to let video data stabilise)
+    print("> Starting UV exposure!")
+    i2c_bus.write_i2c_block_data(DLPC1438_addr, 0xC1, [0x00, 0x05, 0xFF, 0xFF])  
+
+
+    
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 import time
-import smbus  # i2c
-from DLPC_1438 import intialise_DLPC1438, switch_mode, Mode
+import smbus  # I2C
+import spidev  # SPI
+from DLPC_1438 import *
 
 
 GPIO.setmode(GPIO.BCM)
@@ -9,6 +10,12 @@ GPIO.setmode(GPIO.BCM)
 try:
     # Initialize I2C (SMBus) on channel 1
     bus = smbus.SMBus(1)
+
+    # Initialise SPI (bus 0, with CE0 as chip select pin)
+    spi = spidev.SpiDev()
+    spi.open(0, 0)
+    spi.max_speed_hz = 500000  # TODO: figure out max speed
+    spi.mode = 0
 
     # Initialise the DLPC1438
     intialise_DLPC1438(bus)
@@ -24,13 +31,26 @@ try:
 
     # enable flood light
     bus.write_i2c_block_data(DLPC1438_addr, 0x16, [0b00001111])
-    time.sleep(3)  # lit for 3s
+    time.sleep(2)  # lit for 2s
     bus.write_i2c_block_data(DLPC1438_addr, 0x16, [0b00000000])
     time.sleep(1)  # back to testpattern (dim)
 
     # and back to standby
     switch_mode(Mode.STANDBY, bus)
 
+    # let's try external print mode now
+    configure_external_print(0, bus)
+    switch_mode(Mode.EXTERNALPRINT, bus)
+
+    # it will take maybe a second for SYS_RDY to go high and we need to wait for that before exposing
+    await_SYS_RDY()  
+    expose_pattern(0, bus)
+
+
+    time.sleep(3)  # wait before cleanup
+
+    # and back to standby
+    switch_mode(Mode.STANDBY, bus)
     GPIO.cleanup()
 
 except KeyboardInterrupt:
