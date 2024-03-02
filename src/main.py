499aa18c5e59d4485cc19efeea771321db9e5515
Nemo Andrea <nemoandrea@gmail.com>
2024-03-02 01:25:29 +0100

    Move to object-oriented and implement buffer swap, pwm, and exposure time
    Big refactor in moving to object oriented formulation for code. Cleaned up the code a bit with some convenience functions. Made into a more appropriate package form. Implemented the SPI buffer swap feature, rather than useing the same buffer every time. Made sure the LED PWM argument actually controls the LED brightness. Made sure the exposure time now actually sets the exposure time.

--- src/main.py
+++ src/main.py
@@ -2,14 +2,14 @@ import RPi.GPIO as GPIO
 import time
 import smbus  # I2C
 import spidev  # SPI
-from DLPC_1438 import *
+from UV_projector.controller import DLPC1438, Mode
 
 
 GPIO.setmode(GPIO.BCM)
 
 try:
     # Initialize I2C (SMBus) on channel 1
-    bus = smbus.SMBus(1)
+    i2c = smbus.SMBus(1)
 
     # Initialise SPI (bus 0, with CE0 as chip select pin)
     spi = spidev.SpiDev()
@@ -18,32 +18,36 @@ try:
     spi.mode = 3  #TODO: figure out which mode is right. See https://e2e.ti.com/support/dlp-products-group/dlp/f/dlp-products-forum/1187357/dlp300s-dlp300s-and-dlpc1438-application-sample
 
     # Initialise the DLPC1438
-    intialise_DLPC1438(bus)
+    DMD = DLPC1438(i2c, spi)
    
-    #  DLPC1438 on Anyubic board address
-    DLPC1438_addr = 0x1B
-
-    mode = bus.read_i2c_block_data(DLPC1438_addr,0x06,1)  # probably standby if just initialised
+    # as an example, let's manually read some i2c information
+    mode = i2c.read_i2c_block_data(DMD.addr,0x06,1)  # probably standby if just initialised
     print(f"we are in mode: {mode}")
 
     # let's try external print mode now
-    configure_external_print(0, bus)
-    switch_mode(Mode.EXTERNALPRINT, bus)
+    DMD.configure_external_print(LED_PWM = 512)
+    DMD.switch_mode(Mode.EXTERNALPRINT)
 
-    # it will take maybe a second for SYS_RDY to go high and we need to wait for that before exposing
-    await_SYS_RDY()  
+    DMD.set_background(0)  # set every pixel to 0 (as a demo) - takes about 6s to transfer data
 
-    set_background(0, spi, bus)
-    time.sleep(0.5)
+    time.sleep(1)
 
-    send_split_image_to_buffer('../media/openMLA_logo_1280x720.png', 640,360, spi, bus)  # send the image data into FPGA buffer over SPI
-    time.sleep(0.1)  # probably not needed   # TODO: remove this or find minimum time?
-    expose_pattern(0, bus)  #  for now expose until it is switched to standby or explicit stop cmd
+    DMD.send_image_to_buffer('../media/openMLA_logo_1280x720.png', 640,360)  # send the image data into FPGA buffer over SPI
+    DMD.expose_pattern(exposed_frames = 200)  #  expose for 200 frames (= 200/60 ~ 3.3s) 
 
-    time.sleep(5)  # wait before cleanup
+    time.sleep(5)  # the pattern should stop before this timer runs out
+
+    DMD.send_image_to_buffer('../media/openMLA_logo_1280x720.png', 0,0)  # send the image data into FPGA buffer over SPI
+    DMD.expose_pattern(exposed_frames = -1)  #  expose for inifinite duration (until we manually stop it)
+
+    time.sleep(1)  # wait one second
+
+    DMD.stop_exposure()  # and then manually stop exposure
+
+    time.sleep(1) 
 
     # and back to standby
-    switch_mode(Mode.STANDBY, bus)
+    DMD.switch_mode(Mode.STANDBY)
     GPIO.cleanup()
 
 except KeyboardInterrupt:
