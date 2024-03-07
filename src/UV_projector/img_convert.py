d030ec1a99e754fd5c9eb8ed563766c78344da5c
Nemo Andrea <nemoandrea@gmail.com>
2024-03-08 00:30:44 +0100

    Remove image inversion
    Img_convert inverted the image, but this was only for testing purposes and has therefore been removed.

--- src/UV_projector/img_convert.py
+++ src/UV_projector/img_convert.py
@@ -3,12 +3,10 @@ import numpy as np
 
 
 def image_to_arr(path):
+    # TODO: run checks on image bit depth and so on
+
     print("-- Converting image to bytes --")
     im_frame = Image.open(path)
-    pixeldata = 255-np.transpose(np.array(im_frame))
-
-    print(pixeldata.shape)
-    print(pixeldata)
-    print("\n")
+    pixeldata = np.transpose(np.array(im_frame))
 
     return  pixeldata
